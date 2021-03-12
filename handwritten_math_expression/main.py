93d2e98657f7c83f1f5cb85c6d2c7b9992848660
ningjis <shenningji@gmail.com>
2021-03-11 16:20:00 -0800

    Update main.py

--- handwritten_math_expression/main.py
+++ handwritten_math_expression/main.py
@@ -5,13 +5,13 @@ from keras.models import load_model
 from keras.preprocessing.image import load_img, img_to_array
 import ImagePreprocessing as ip
 
+labels = []
+positions = []
 numeric = string.digits + "e" + "pi"
 inputpath = './data/testimg/testinkml_1.png'
-savepath = './data/testimg'
+ = './data/testimg'
 
-#input image segmenatation and resize
-def inputImageSegmentation(filepath,savepath):
-    ip.predictImageSegementation(inputpath,savepath)
+ip.predictImageSegementation(inputpath,savepath)
 
 # input the path of a single image, output a single label
 def predict_single_label(input_img_path,model,label_map):
@@ -33,7 +33,6 @@ def predict_single_label(input_img_path,model,label_map):
     predicted_class_indices=np.argmax(pred, axis =1 )
     class_indices = dict((v, k) for k, v in class_indices.items())
     predictions = [class_indices[k] for k in predicted_class_indices]
-    print(pred)
     print(input_img_path)
     print ("pred",predictions)
     return predictions
@@ -43,26 +42,34 @@ class ModelInputError(Exception):
         self.msg = "Unable to file segments and/or location file"
 
 filepath = "/trainPNGSeg/Shareddrives/515_handwritten/test_2"
-model = load_model('LeNetModel_v3.h5')
-label_map = 'label_map_v3.npy'
-label = []
 
-for root,dir,files in os.walk(filepath):
-  for file in sorted(files):
-    if file.endswith('png'):
-      single_label = predict_single_label(os.path.join(root, file),model,label_map)
-      # single_label[0] is the value, [1] is the tag (numeric or operator)
-      if single_label[0] in numeric:
-        single_label.append("numeric")
+#input a folder with all segments.npg and a position.pkl
+#output a list of labels and a list of positions
+def write_labels_for_all_segs(filepath):
+  model = load_model('LeNetModel_v3.h5')
+  label_map = 'label_map_v3.npy'
+  label = []
+
+  for root,dir,files in os.walk(filepath):
+    for file in sorted(files):
+      if file.endswith('png'):
+        single_label = predict_single_label(os.path.join(root, file),model,label_map)
+        # single_label[0] is the value, [1] is the tag (numeric or operator)
+        if single_label[0] in numeric:
+          single_label.append("numeric")
+        else:
+          single_label.append("operator")
+        label.append(single_label)
+      elif file.endswith('pkl'):
+        print("pkl:",file)
+        with open(os.path.join(root, file), 'rb') as f:
+          positions = pickle.load(f)
       else:
-        single_label.append("operator")
-      label.append(single_label)
-    elif file.endswith('pkl'):
-      print("pkl:",file)
-      with open(os.path.join(root, file), 'rb') as f:
-        positions = pickle.load(f)
-    else:
-      raise ModelInputError
+        raise ModelInputError
 
-print("labels:", label)
+  return label,positions
+
+
+labels,positions = write_labels_for_all_segs(savepath)
+print("labels:", labels)
 print("positions:",positions)

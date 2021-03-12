d05fe9ac5edf751d94a07b0e2e640ff7a3fa7d30
ningjis <shenningji@gmail.com>
2021-03-11 20:36:35 -0800

    Create stringMathJaxConverter.py
    input is a string

---
+++ handwritten_math_expression/stringMathJaxConverter.py
@@ -0,0 +1,25 @@
+import string
+import os.path
+from os import path
+
+def convertXmlMathjax(string):
+    result = stringToMathJax(string)
+    if path.exists("MathJaxResult.txt"):
+        os.remove("MathJaxResult.txt")
+    f = open("MathJaxResult.txt", "w")
+    f.write(str(result))
+    f.close()
+
+# return the MathJax expression from the parsed xml tree
+def stringToMathJax(string):
+    #string.replace(old, new)
+    result = string.replace("pi","\pi")
+    result = result.replace("times","\\times")
+    result = result.replace("/" ,"\div")
+    result = result.replace("neq","\\neq")
+    result = result.replace("geq" ,"\\geq")
+    result = result.replace("gt" ,"\\gt")
+    result = result.replace("leq" ,"\\leq")
+    result = result.replace("lt" ,"\\lt")
+    result = result.replace("pm" ,"\\pm")
+    return result

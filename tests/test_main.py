a7961ec4919e8f735dffec5c52dd7344079ec21e
ningjis <shenningji@gmail.com>
2021-03-12 19:37:30 -0800

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,10 +8,10 @@ class TestMain(unittest.TestCase):
     @patch('handwritten_math_expression.main.predict_single_label')
     def test_file_called(self, mock_predict_single_label):
         print("test_file_called")
-        mock_predict_single_label.return_value = "0"
+        mock_predict_single_label.return_value = "8"
         hme.write_labels_for_all_segs("test_1")
         # self.assertTrue(mock_newsgroup_file.called)
-        self.assertEqual(mock_predict_single_label.call_count,5)
+        self.assertEqual(mock_predict_single_label.call_count,8)
 
 if __name__ == '__main__':
     unittest.main()

3c820fda0af3bf35eef16a8af38b0cedc0897200
ningjis <shenningji@gmail.com>
2021-03-13 00:23:43 -0800

    Update test_stringMathJaxConverter.py

--- tests/test_stringMathJaxConverter.py
+++ tests/test_stringMathJaxConverter.py
@@ -5,11 +5,11 @@ import os.path
 class TestCalculation(unittest.TestCase):
 
     def testMathJaxComponent(self):
-        self.assertEqual(MJAX.stringToMathJax("1+2"), "1+2")
-        self.assertEqual(MJAX.stringToMathJax("1/2"), "1\\div2")
-        self.assertEqual(MJAX.stringToMathJax("1*2"), "1\\times2")
-        self.assertEqual(MJAX.stringToMathJax("1geq2"), "1\\geq2")
-        self.assertEqual(MJAX.stringToMathJax("(a*b)neqc"), "(a\\timesb)\\neqc")
+        self.assertEqual(MJAX.stringToMathJax("1+2"), "1 + 2")
+        self.assertEqual(MJAX.stringToMathJax("1/2"), "1 \\div 2")
+        self.assertEqual(MJAX.stringToMathJax("1*2"), "1 \\times 2")
+        self.assertEqual(MJAX.stringToMathJax("1geq2"), "1 \\geq 2")
+        self.assertEqual(MJAX.stringToMathJax("(a*b)neqc"), "(a \\times b) \\neq c")
         self.assertEqual(MJAX.stringToMathJax("i^2"), "i^{ 2 }")
 
 if __name__ == '__main__':

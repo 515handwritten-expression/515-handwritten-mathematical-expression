5dadfe66423c9871dd3eca0a52328a5665141a41
ningjis <shenningji@gmail.com>
2021-03-13 00:12:14 -0800

    update

--- handwritten_math_expression/stringMathJaxConverter.py
+++ handwritten_math_expression/stringMathJaxConverter.py
@@ -1,4 +1,4 @@
-import string
+import string,re
 import os.path
 from os import path
 
@@ -12,14 +12,27 @@ def convertMathjax(string):
 
 # return the MathJax expression from the parsed xml tree
 def stringToMathJax(string):
+    result = ""
+    str_seg = re.findall(r"([a-z]+|\d|[-+()/*^])", string)
+    i=0
+    for i in range(len(str_seg)+2):
+        element = str_seg[i]
+        if(element == "^"):
+          str_seg.insert(i+1, "{ ")
+          str_seg.insert(i+3, " }")
+    for ele in str_seg:
+      result += ele
     #string.replace(old, new)
-    result = string.replace("pi","\\pi")
-    result = result.replace("*","\\times")
-    result = result.replace("/" ,"\\div")
-    result = result.replace("neq","\\neq")
-    result = result.replace("geq" ,"\\geq")
-    result = result.replace("gt" ,"\\gt")
-    result = result.replace("leq" ,"\\leq")
-    result = result.replace("lt" ,"\\lt")
-    result = result.replace("pm" ,"\\pm")
+    result = result.replace("+"," + ")
+    result = result.replace("-"," - ")
+    result = result.replace("pi"," \\pi ")
+    result = result.replace("*"," \\times ")
+    result = result.replace("/" ," \\div ")
+    result = result.replace("neq"," \\neq ")
+    result = result.replace("geq" ," \\geq ")
+    result = result.replace("gt" ," \\gt ")
+    result = result.replace("leq" ," \\leq ")
+    result = result.replace("lt" ," \\lt ")
+    result = result.replace("pm" ," \\pm ")
+
     return result
--- tests/test_stringMathJaxConverter.py
+++ tests/test_stringMathJaxConverter.py
@@ -10,7 +10,7 @@ class TestCalculation(unittest.TestCase):
         self.assertEqual(MJAX.stringToMathJax("1*2"), "1\\times2")
         self.assertEqual(MJAX.stringToMathJax("1geq2"), "1\\geq2")
         self.assertEqual(MJAX.stringToMathJax("(a*b)neqc"), "(a\\timesb)\\neqc")
-        self.assertEqual(MJAX.stringToMathJax("i^2"), "i^2")
+        self.assertEqual(MJAX.stringToMathJax("i^2"), "i^{ 2 }")
 
 if __name__ == '__main__':
     unittest.main()
